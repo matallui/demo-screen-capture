0705efa9434ff1aea787c68c093c06c0a09599cb
Luis Taniça <ltanica@mersive.com>
2022-08-24 10:08:38 -0600

    plugin: add withIosBroadcastExtension

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,9 +1,10 @@
 import { ConfigPlugin, withPlugins } from '@expo/config-plugins';
 
 import withAndroidNotifeeService from './withAndroidNotifeeService';
+import withIosBroadcastExtension from './withIosBroadcastExtension';
 
 const withScreenCapture: ConfigPlugin = (config) => {
-  return withPlugins(config, [withAndroidNotifeeService]);
+  return withPlugins(config, [withAndroidNotifeeService, withIosBroadcastExtension]);
 };
 
 export default withScreenCapture;
